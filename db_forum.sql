3056a3e549a483ef2f235c512ae11a33fbddac2e
bkablam11 <bkablam11@gmail.com>
2022-04-19 02:54:53 +0000

    adding

--- db_forum.sql
+++ db_forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mar. 19 avr. 2022 à 03:08
+-- Généré le : mar. 19 avr. 2022 à 04:53
 -- Version du serveur :  10.4.18-MariaDB
 -- Version de PHP : 8.0.3
 
@@ -28,12 +28,25 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `message` (
-  `id_messge` int(11) NOT NULL,
+  `id_message` int(11) NOT NULL,
   `texte_message` text NOT NULL,
   `date_message` date NOT NULL,
   `heure_message` time NOT NULL,
   `id_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `message`
+--
+
+INSERT INTO `message` (`id_message`, `texte_message`, `date_message`, `heure_message`, `id_user`) VALUES
+(1, 'lorum', '2022-04-19', '22:18:31', 7),
+(2, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit pariatur est, placeat modi consectetur quae ex commodi sed, numquam expedita omnis veniam, ratione aliquid quisquam ipsam temporibus voluptatibus aut vel.', '2022-04-19', '04:38:00', 8),
+(3, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit pariatur est, placeat modi consectetur quae ex commodi sed, numquam expedita omnis veniam, ratione aliquid quisquam ipsam temporibus voluptatibus aut vel.', '2022-04-19', '02:39:00', 8),
+(4, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit pariatur est, placeat modi consectetur quae ex commodi sed, numquam expedita omnis veniam, ratione aliquid quisquam ipsam temporibus voluptatibus aut vel.', '2022-04-19', '00:39:42', 7),
+(5, 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit pariatur est, placeat modi consectetur quae ex commodi sed, numquam expedita omnis veniam, ratione aliquid quisquam ipsam temporibus voluptatibus aut vel.', '2022-04-19', '04:40:00', 8),
+(6, '', '2022-04-19', '04:50:00', 9),
+(7, 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit atque rerum exercitationem harum aliquam, molestias pariatur at laborum magnam eveniet, delectus est totam quae enim aliquid fugiat commodi cum error.', '2022-04-19', '04:51:00', 9);
 
 -- --------------------------------------------------------
 
@@ -50,6 +63,15 @@ CREATE TABLE `utilisateur` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Déchargement des données de la table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_user`, `nom_user`, `prenom_user`, `email_user`, `pw_user`) VALUES
+(7, 'Blanchard', 'Kablam', 'bkablam11@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220'),
+(8, 'nom1', 'prenom1', 'email1@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220'),
+(9, 'nom2', 'prenom2', 'email2@gmail.com', '7110eda4d09e062aa5e4a390b0a572ac0d2c0220');
+
+--
 -- Index pour les tables déchargées
 --
 
@@ -57,7 +79,7 @@ CREATE TABLE `utilisateur` (
 -- Index pour la table `message`
 --
 ALTER TABLE `message`
-  ADD PRIMARY KEY (`id_messge`);
+  ADD PRIMARY KEY (`id_message`);
 
 --
 -- Index pour la table `utilisateur`
@@ -73,13 +95,13 @@ ALTER TABLE `utilisateur`
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id_messge` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_message` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
